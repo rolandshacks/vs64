a3ae6ee10a2df2b1aeaa5b0b67223c19ce7e05d9
Steve Reuter <77981312+stevereuter@users.noreply.github.com>
2026-06-27 16:49:14 -0400

    Fix THEN label parsing by removing premature token short-circuit

--- tools/bclib/compiler.py
+++ tools/bclib/compiler.py
@@ -1073,10 +1073,6 @@ class BasicCompiler:
                         ofs += 1
                         if ofs < len(line):
                             c = line[ofs]
-                        if last_was_jump == 0xA7 and len(label) >= 2:
-                            # handle 'THEN TOKEN' instead of 'THEN label'
-                            tmp_token, tmp_token_code, _ = self.match_token(label)
-                            if tmp_token and tmp_token_code != 0xCB: break # found BASIC token, stop parsing label
                     else:
                         ofs_old = ofs # backup position after identifier
 
